300c2e0eea54458167f2c675181aadce0f33a18c
Asherlee Levy <asherleelevy@gmail.com>
2018-07-20 03:39:19 -0400

    Added UI to welcome screen w/out auto layout and created a custom class for button border

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,28 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Asherlee Levy on 7/20/18.
+//  Copyright © 2018 Asherlee Levy. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor =
+        UIColor.white.cgColor
+    }
+
+}
