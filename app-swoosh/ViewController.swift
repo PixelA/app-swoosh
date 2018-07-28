9a1c6f90ee22beb89a8df7cb7863d11a79757fd9
Asherlee Levy <asherleelevy@gmail.com>
2018-07-28 00:45:16 -0400

    Added segue from back buttons and completed the stack views

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
 
 }
 
