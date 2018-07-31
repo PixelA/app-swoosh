3dafbb8b41e852996253547fe4a38f35f4262133
Asherlee Levy <asherleelevy@gmail.com>
2018-07-31 01:36:33 -0400

    Created struct and function for league selection

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
     @IBOutlet weak var nextBtn: BorderButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
@@ -25,12 +29,19 @@ class LeagueVC: UIViewController {
     
     
     @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
     }
     
     @IBAction func onWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "womens")
     }
     
     @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
     override func didReceiveMemoryWarning() {
